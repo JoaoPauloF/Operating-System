b0cae4b1314bb65ef05463f1522a79b6e5f77273
JoaoPauloF <andradejp@hotmail.com>
2018-01-09 21:20:14 -0200

    Corrections in start.s

--- kernel/v0.1/src/start.s
+++ kernel/v0.1/src/start.s
@@ -1,4 +1,4 @@
-.extern kernel_main
+.extern _kmain
 .global_start
 .set MB_MAGIC, 0x1BADB002
 .set MB_FLAGS, (1<<0) | (1<<1)
@@ -10,18 +10,20 @@
 	.long MB_FLAGS
 	.long MB_CHECKSUM
 
+.section .text
+.global	_start
+.type _start, @function
+_start:
+		mov $stack_top, $esp
+		call _kmain
+		
+		hang:
+			cli		
+			hlt
+			jmp hang
 .section .bss
 	.align 16
 	stack_bottom:
 		.skip 4096 
 	stack_top:
 
-.section .text
-	start:
-		mov $stack_top, $esp
-		call kernel_main
-		
-		hang:
-			cli		
-			hlt
-			jmp hang
